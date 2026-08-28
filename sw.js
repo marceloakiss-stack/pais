a47ef944e8d181287d20a760b2cd42009c7af45f
Adrian <marceloakiss@gmail.com>
2026-08-28 10:01:37 +0200

    Mejora diseño y recolección de datos

--- sw.js
+++ sw.js
@@ -1,16 +1,28 @@
-const CACHE_NAME = 'atlas-local-v2';
+const CACHE_NAME = 'atlas-local-v3';
 const APP_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+    event.waitUntil(
+        caches.keys()
+            .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+            .then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-    if (new URL(event.request.url).origin === self.location.origin) {
-        event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
-    }
+    if (new URL(event.request.url).origin !== self.location.origin) return;
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                const copy = response.clone();
+                caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
 });
